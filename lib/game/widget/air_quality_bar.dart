ba986294f528f46bdb8d15065df0470dd22027ee
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-07-29 16:03:22 +0700

    .

--- lib/game/widget/air_quality_bar.dart
+++ lib/game/widget/air_quality_bar.dart
@@ -25,52 +25,77 @@ class AirQualityBar extends StatelessWidget {
         builder: (context, snapshot) {
           AirQuality quality = AirQuality.good;
           int aqi = 0;
+          num temperature = 0;
+
           if (snapshot.data != null) {
             if (snapshot.data!.airVisualResult != null) {
               aqi = snapshot.data!.airVisualResult!.currentPollution.aqi;
+              temperature =
+                  snapshot.data!.airVisualResult!.currentWeather.temperature;
             }
             quality = snapshot.data!.airQuality;
           }
-          return Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          return Column(
             children: [
-              Image.asset(
-                "assets/icon/air.png",
-                filterQuality: FilterQuality.none,
-                width: 40,
-                height: 40,
-                fit: BoxFit.contain,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "assets/icon/air.png",
+                    filterQuality: FilterQuality.none,
+                    width: 40,
+                    height: 40,
+                    fit: BoxFit.contain,
+                  ),
+                  Text(" AQI: $aqi | ",
+                      style: TextStyle(
+                        fontFamily: "Pixelate",
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1,
+                        fontSize: 14,
+                        color: Colors.white,
+                      )),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                      BoxShadow(color: Colors.grey, offset: Offset(0, 3))
+                    ]),
+                    padding: EdgeInsets.all(3),
+                    child: Text(
+                      _qualityString[quality.index],
+                      style: TextStyle(
+                          fontFamily: "Pixelate",
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.5,
+                          fontSize: 14,
+                          color: quality == AirQuality.good
+                              ? Colors.greenAccent.shade700
+                              : quality == AirQuality.moderate
+                                  ? primaryYellowLighter
+                                  : quality == AirQuality.unhealthy
+                                      ? Colors.red
+                                      : Colors.red.shade800),
+                    ),
+                  ),
+                ],
               ),
-              Text(" AQI: $aqi | ",
-                  style: TextStyle(
-                    fontFamily: "Pixelate",
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1,
-                    fontSize: 14,
-                    color: Colors.white,
-                  )),
-              Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(color: Colors.grey, offset: Offset(0, 3))
-                ]),
-                padding: EdgeInsets.all(3),
-                child: Text(
-                  _qualityString[quality.index],
-                  style: TextStyle(
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Image.asset(
+                  "assets/icon/thermometer.png",
+                  filterQuality: FilterQuality.none,
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.contain,
+                ),
+                Text("Temporature: $temperature C",
+                    style: TextStyle(
                       fontFamily: "Pixelate",
                       fontWeight: FontWeight.bold,
-                      letterSpacing: 0.5,
+                      letterSpacing: 1,
                       fontSize: 14,
-                      color: quality == AirQuality.good
-                          ? Colors.greenAccent.shade700
-                          : quality == AirQuality.moderate
-                              ? primaryYellowLighter
-                              : quality == AirQuality.unhealthy
-                                  ? Colors.red
-                                  : Colors.red.shade800),
-                ),
-              ),
+                      color: Colors.white,
+                    )),
+              ])
             ],
           );
         });
